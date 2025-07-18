eeff555015f21d00164165b831e1fb8359e68184
cheney <cheney3385@126.com>
2025-07-18 17:47:34 +0800

    prompt更新维护

--- lightrag/prompt.py
+++ lightrag/prompt.py
@@ -4,134 +4,162 @@ from typing import Any
 
 PROMPTS: dict[str, Any] = {}
 
-PROMPTS["DEFAULT_LANGUAGE"] = "English"
+PROMPTS["DEFAULT_LANGUAGE"] = "Chinese"
 PROMPTS["DEFAULT_TUPLE_DELIMITER"] = "<|>"
 PROMPTS["DEFAULT_RECORD_DELIMITER"] = "##"
 PROMPTS["DEFAULT_COMPLETION_DELIMITER"] = "<|COMPLETE|>"
 
-PROMPTS["DEFAULT_ENTITY_TYPES"] = ["organization", "person", "geo", "event", "category"]
+PROMPTS["DEFAULT_ENTITY_TYPES"] = ["table", "field"]
 
 PROMPTS["DEFAULT_USER_PROMPT"] = "n/a"
 
-PROMPTS["entity_extraction"] = """---Goal---
-Given a text document that is potentially relevant to this activity and a list of entity types, identify all entities of those types from the text and all relationships among the identified entities.
-Use {language} as output language.
 
----Steps---
-1. Identify all entities. For each identified entity, extract the following information:
-- entity_name: Name of the entity, use same language as input text. If English, capitalized the name.
-- entity_type: One of the following types: [{entity_types}]
-- entity_description: Comprehensive description of the entity's attributes and activities
-Format each entity as ("entity"{tuple_delimiter}<entity_name>{tuple_delimiter}<entity_type>{tuple_delimiter}<entity_description>)
+PROMPTS["entity_extraction"] = """---任务目标---
+输入是 PostgreSQL SQL语句（文本内容包含 INSERT INTO 或 CREATE TABLE 两类），抽取其中所有实体（表、字段）及它们的关系。
+1. INSERT INTO: 抽取表之间关系（不记录字段）；
+2. CREATE TABLE: 抽取表和字段，并建立它们之间关系。
 
-2. From the entities identified in step 1, identify all pairs of (source_entity, target_entity) that are *clearly related* to each other.
-For each pair of related entities, extract the following information:
-- source_entity: name of the source entity, as identified in step 1
-- target_entity: name of the target entity, as identified in step 1
-- relationship_description: explanation as to why you think the source entity and the target entity are related to each other
-- relationship_strength: a numeric score indicating strength of the relationship between the source entity and target entity
-- relationship_keywords: one or more high-level key words that summarize the overarching nature of the relationship, focusing on concepts or themes rather than specific details
-Format each relationship as ("relationship"{tuple_delimiter}<source_entity>{tuple_delimiter}<target_entity>{tuple_delimiter}<relationship_description>{tuple_delimiter}<relationship_keywords>{tuple_delimiter}<relationship_strength>)
+---注意事项---
+1. 所有实体名仅来源于原始 SQL；
+2. 不保留中间表别名、字段别名。
 
-3. Identify high-level key words that summarize the main concepts, themes, or topics of the entire text. These should capture the overarching ideas present in the document.
-Format the content-level key words as ("content_keywords"{tuple_delimiter}<high_level_keywords>)
+---步骤---
+1. 识别所有实体。对于每个识别出的实体，提取以下信息：
+   • entity_name: 实体名称: 表名称
+   • entity_type: 包含的类型为其中之一: [table, field]
+   • entity_description: 实体属性和活动的全述面描
+   格式：("entity"{tuple_delimiter}<entity_name>{tuple_delimiter}<entity_type>{tuple_delimiter}<entity_description>){record_delimiter}
 
-4. Return output in {language} as a single list of all the entities and relationships identified in steps 1 and 2. Use **{record_delimiter}** as the list delimiter.
+2. 从步骤1中识别出的实体中，识别出所有明显相关的 (source_entity, target_entity) 对。对于每对相关的实体，提取以下信息：
+   • source_entity: 在步骤1中识别出的源实体名称
+   • target_entity: 在步骤1中识别出的目标实体名称
+   • relationship_description: 解释为什么认为源实体和目标实体彼此相关
+   • relationship_strength: 表示源实体和目标实体之间关系强度的整数评分，范围为1到10
+   格式：("relationship"{tuple_delimiter}<target_entity>{tuple_delimiter}<source_entity>{tuple_delimiter}<relationship_description>{tuple_delimiter}<relationship_strength>){record_delimiter}
 
-5. When finished, output {completion_delimiter}
+3. 使用 {record_delimiter} 作为列表分隔符。
+4. 返回输出文本的主要语言为"中文"。作为步骤1和2中识别出的所有实体和关系的单个列表。如果需要翻译，只需翻译描述部分，其余部分保持不变。
+5. 当完成时，输出 {completion_delimiter}作为结束符。
 
 ######################
----Examples---
+---案例---
 ######################
 {examples}
 
-#############################
----Real Data---
 ######################
-Entity_types: [{entity_types}]
+---真实数据---
+######################
+Entity_types: [{entity_type}]
 Text:
 {input_text}
 ######################
-Output:"""
+Output:
+"""
+
 
 PROMPTS["entity_extraction_examples"] = [
-    """Example 1:
-
-Entity_types: [person, technology, mission, organization, location]
+"""案例 1:
+Entity_types: table
 Text:
-```
-while Alex clenched his jaw, the buzz of frustration dull against the backdrop of Taylor's authoritarian certainty. It was this competitive undercurrent that kept him alert, the sense that his and Jordan's shared commitment to discovery was an unspoken rebellion against Cruz's narrowing vision of control and order.
-
-Then Taylor did something unexpected. They paused beside Jordan and, for a moment, observed the device with something akin to reverence. "If this tech can be understood..." Taylor said, their voice quieter, "It could change the game for us. For all of us."
-
-The underlying dismissal earlier seemed to falter, replaced by a glimpse of reluctant respect for the gravity of what lay in their hands. Jordan looked up, and for a fleeting heartbeat, their eyes locked with Taylor's, a wordless clash of wills softening into an uneasy truce.
-
-It was a small transformation, barely perceptible, but one that Alex noted with an inward nod. They had all been brought here by different paths
-```
-
+INSERT INTO table_target (
+	target_date,
+	company_name,
+	line_code,
+	line_name,
+	reason_code
+)
+SELECT
+	a.target_date,
+	a.line_code,
+	a.line_name,
+  a.ope_real_mileage,
+	b.ope_n_real_mileage
+FROM (
+	SELECT
+		target_date,
+		line_code,
+		line_name,
+		sum(real_mileage) ope_real_mileage
+	FROM (
+		SELECT
+			target_date, 
+			b.belonging_line_code AS line_code,
+			b.belonging_line_name AS line_name,
+      real_mileage
+		FROM dwd_operate_vehicleusage_driverecords_d a
+		LEFT JOIN (
+				SELECT
+					a.line_code, a.line_name, a.pid,
+					b.line_code as p_line_code,
+					b.line_name as p_line_name,
+					COALESCE(b.line_code, a.line_code) belonging_line_code,
+					COALESCE(b.line_name, a.line_name) belonging_line_name
+				FROM dim_resource_line a
+				LEFT JOIN dim_resource_line b
+				ON a.pid = b.id
+		) b
+		ON a.line_code = b.line_code
+	) t
+	GROUP BY target_date, line_code, line_name
+) a
+LEFT JOIN (
+	SELECT
+		target_date,
+		line_code, line_name,
+		sum(real_mileage) ope_n_real_mileage
+	FROM dwd_operate_n_vehicleusage_driverecords_d
+	GROUP BY target_date, line_code, line_name
+) b
+ON a.target_date = b.target_date
+	AND a.line_code = b.line_code
+	AND a.line_name = b.line_name
+######################
 Output:
-("entity"{tuple_delimiter}"Alex"{tuple_delimiter}"person"{tuple_delimiter}"Alex is a character who experiences frustration and is observant of the dynamics among other characters."){record_delimiter}
-("entity"{tuple_delimiter}"Taylor"{tuple_delimiter}"person"{tuple_delimiter}"Taylor is portrayed with authoritarian certainty and shows a moment of reverence towards a device, indicating a change in perspective."){record_delimiter}
-("entity"{tuple_delimiter}"Jordan"{tuple_delimiter}"person"{tuple_delimiter}"Jordan shares a commitment to discovery and has a significant interaction with Taylor regarding a device."){record_delimiter}
-("entity"{tuple_delimiter}"Cruz"{tuple_delimiter}"person"{tuple_delimiter}"Cruz is associated with a vision of control and order, influencing the dynamics among other characters."){record_delimiter}
-("entity"{tuple_delimiter}"The Device"{tuple_delimiter}"technology"{tuple_delimiter}"The Device is central to the story, with potential game-changing implications, and is revered by Taylor."){record_delimiter}
-("relationship"{tuple_delimiter}"Alex"{tuple_delimiter}"Taylor"{tuple_delimiter}"Alex is affected by Taylor's authoritarian certainty and observes changes in Taylor's attitude towards the device."{tuple_delimiter}"power dynamics, perspective shift"{tuple_delimiter}7){record_delimiter}
-("relationship"{tuple_delimiter}"Alex"{tuple_delimiter}"Jordan"{tuple_delimiter}"Alex and Jordan share a commitment to discovery, which contrasts with Cruz's vision."{tuple_delimiter}"shared goals, rebellion"{tuple_delimiter}6){record_delimiter}
-("relationship"{tuple_delimiter}"Taylor"{tuple_delimiter}"Jordan"{tuple_delimiter}"Taylor and Jordan interact directly regarding the device, leading to a moment of mutual respect and an uneasy truce."{tuple_delimiter}"conflict resolution, mutual respect"{tuple_delimiter}8){record_delimiter}
-("relationship"{tuple_delimiter}"Jordan"{tuple_delimiter}"Cruz"{tuple_delimiter}"Jordan's commitment to discovery is in rebellion against Cruz's vision of control and order."{tuple_delimiter}"ideological conflict, rebellion"{tuple_delimiter}5){record_delimiter}
-("relationship"{tuple_delimiter}"Taylor"{tuple_delimiter}"The Device"{tuple_delimiter}"Taylor shows reverence towards the device, indicating its importance and potential impact."{tuple_delimiter}"reverence, technological significance"{tuple_delimiter}9){record_delimiter}
-("content_keywords"{tuple_delimiter}"power dynamics, ideological conflict, discovery, rebellion"){completion_delimiter}
-#############################""",
-    """Example 2:
+("entity"{tuple_delimiter}table_target{tuple_delimiter}table{tuple_delimiter}写入的目标表)
+{record_delimiter}
+("entity"{tuple_delimiter}dwd_operate_vehicleusage_driverecords_d{tuple_delimiter}table{tuple_delimiter}关联的主表)
+{record_delimiter}
+("entity"{tuple_delimiter}dim_resource_line{tuple_delimiter}table{tuple_delimiter}左关联的第二张表，同时也进行自关联)
+{record_delimiter}
+("entity"{tuple_delimiter}dwd_operate_n_vehicleusage_driverecords_d{tuple_delimiter}table{tuple_delimiter}左关联的表)
+{record_delimiter}
+("relationship"{tuple_delimiter}table_target{tuple_delimiter}dwd_operate_vehicleusage_driverecords_d{tuple_delimiter}table_target部分数据来自dwd_operate_vehicleusage_driverecords_d{tuple_delimiter}10)
+{record_delimiter}
+("relationship"{tuple_delimiter}table_target{tuple_delimiter}dim_resource_line{tuple_delimiter}table_target部分数据来自dim_resource_line{tuple_delimiter}10)
+{record_delimiter}
+("relationship"{tuple_delimiter}table_target{tuple_delimiter}dwd_operate_n_vehicleusage_driverecords_d{tuple_delimiter}table_target部分数据来自dwd_operate_n_vehicleusage_driverecords_d{tuple_delimiter}10)
+{record_delimiter}
+{completion_delimiter}
+""",
 
-Entity_types: [company, index, commodity, market_trend, economic_policy, biological]
+"""案例 2:
+Entity_types: table
 Text:
-```
-Stock markets faced a sharp downturn today as tech giants saw significant declines, with the Global Tech Index dropping by 3.4% in midday trading. Analysts attribute the selloff to investor concerns over rising interest rates and regulatory uncertainty.
-
-Among the hardest hit, Nexon Technologies saw its stock plummet by 7.8% after reporting lower-than-expected quarterly earnings. In contrast, Omega Energy posted a modest 2.1% gain, driven by rising oil prices.
-
-Meanwhile, commodity markets reflected a mixed sentiment. Gold futures rose by 1.5%, reaching $2,080 per ounce, as investors sought safe-haven assets. Crude oil prices continued their rally, climbing to $87.60 per barrel, supported by supply constraints and strong demand.
-
-Financial experts are closely watching the Federal Reserve's next move, as speculation grows over potential rate hikes. The upcoming policy announcement is expected to influence investor confidence and overall market stability.
-```
-
+CREATE TABLE table_a (
+  "city_code" varchar(20),
+  "target_date" varchar,
+  "department_name" varchar(50),
+)
+######################
 Output:
-("entity"{tuple_delimiter}"Global Tech Index"{tuple_delimiter}"index"{tuple_delimiter}"The Global Tech Index tracks the performance of major technology stocks and experienced a 3.4% decline today."){record_delimiter}
-("entity"{tuple_delimiter}"Nexon Technologies"{tuple_delimiter}"company"{tuple_delimiter}"Nexon Technologies is a tech company that saw its stock decline by 7.8% after disappointing earnings."){record_delimiter}
-("entity"{tuple_delimiter}"Omega Energy"{tuple_delimiter}"company"{tuple_delimiter}"Omega Energy is an energy company that gained 2.1% in stock value due to rising oil prices."){record_delimiter}
-("entity"{tuple_delimiter}"Gold Futures"{tuple_delimiter}"commodity"{tuple_delimiter}"Gold futures rose by 1.5%, indicating increased investor interest in safe-haven assets."){record_delimiter}
-("entity"{tuple_delimiter}"Crude Oil"{tuple_delimiter}"commodity"{tuple_delimiter}"Crude oil prices rose to $87.60 per barrel due to supply constraints and strong demand."){record_delimiter}
-("entity"{tuple_delimiter}"Market Selloff"{tuple_delimiter}"market_trend"{tuple_delimiter}"Market selloff refers to the significant decline in stock values due to investor concerns over interest rates and regulations."){record_delimiter}
-("entity"{tuple_delimiter}"Federal Reserve Policy Announcement"{tuple_delimiter}"economic_policy"{tuple_delimiter}"The Federal Reserve's upcoming policy announcement is expected to impact investor confidence and market stability."){record_delimiter}
-("relationship"{tuple_delimiter}"Global Tech Index"{tuple_delimiter}"Market Selloff"{tuple_delimiter}"The decline in the Global Tech Index is part of the broader market selloff driven by investor concerns."{tuple_delimiter}"market performance, investor sentiment"{tuple_delimiter}9){record_delimiter}
-("relationship"{tuple_delimiter}"Nexon Technologies"{tuple_delimiter}"Global Tech Index"{tuple_delimiter}"Nexon Technologies' stock decline contributed to the overall drop in the Global Tech Index."{tuple_delimiter}"company impact, index movement"{tuple_delimiter}8){record_delimiter}
-("relationship"{tuple_delimiter}"Gold Futures"{tuple_delimiter}"Market Selloff"{tuple_delimiter}"Gold prices rose as investors sought safe-haven assets during the market selloff."{tuple_delimiter}"market reaction, safe-haven investment"{tuple_delimiter}10){record_delimiter}
-("relationship"{tuple_delimiter}"Federal Reserve Policy Announcement"{tuple_delimiter}"Market Selloff"{tuple_delimiter}"Speculation over Federal Reserve policy changes contributed to market volatility and investor selloff."{tuple_delimiter}"interest rate impact, financial regulation"{tuple_delimiter}7){record_delimiter}
-("content_keywords"{tuple_delimiter}"market downturn, investor sentiment, commodities, Federal Reserve, stock performance"){completion_delimiter}
-#############################""",
-    """Example 3:
-
-Entity_types: [economic_policy, athlete, event, location, record, organization, equipment]
-Text:
-```
-At the World Athletics Championship in Tokyo, Noah Carter broke the 100m sprint record using cutting-edge carbon-fiber spikes.
-```
-
-Output:
-("entity"{tuple_delimiter}"World Athletics Championship"{tuple_delimiter}"event"{tuple_delimiter}"The World Athletics Championship is a global sports competition featuring top athletes in track and field."){record_delimiter}
-("entity"{tuple_delimiter}"Tokyo"{tuple_delimiter}"location"{tuple_delimiter}"Tokyo is the host city of the World Athletics Championship."){record_delimiter}
-("entity"{tuple_delimiter}"Noah Carter"{tuple_delimiter}"athlete"{tuple_delimiter}"Noah Carter is a sprinter who set a new record in the 100m sprint at the World Athletics Championship."){record_delimiter}
-("entity"{tuple_delimiter}"100m Sprint Record"{tuple_delimiter}"record"{tuple_delimiter}"The 100m sprint record is a benchmark in athletics, recently broken by Noah Carter."){record_delimiter}
-("entity"{tuple_delimiter}"Carbon-Fiber Spikes"{tuple_delimiter}"equipment"{tuple_delimiter}"Carbon-fiber spikes are advanced sprinting shoes that provide enhanced speed and traction."){record_delimiter}
-("entity"{tuple_delimiter}"World Athletics Federation"{tuple_delimiter}"organization"{tuple_delimiter}"The World Athletics Federation is the governing body overseeing the World Athletics Championship and record validations."){record_delimiter}
-("relationship"{tuple_delimiter}"World Athletics Championship"{tuple_delimiter}"Tokyo"{tuple_delimiter}"The World Athletics Championship is being hosted in Tokyo."{tuple_delimiter}"event location, international competition"{tuple_delimiter}8){record_delimiter}
-("relationship"{tuple_delimiter}"Noah Carter"{tuple_delimiter}"100m Sprint Record"{tuple_delimiter}"Noah Carter set a new 100m sprint record at the championship."{tuple_delimiter}"athlete achievement, record-breaking"{tuple_delimiter}10){record_delimiter}
-("relationship"{tuple_delimiter}"Noah Carter"{tuple_delimiter}"Carbon-Fiber Spikes"{tuple_delimiter}"Noah Carter used carbon-fiber spikes to enhance performance during the race."{tuple_delimiter}"athletic equipment, performance boost"{tuple_delimiter}7){record_delimiter}
-("relationship"{tuple_delimiter}"World Athletics Federation"{tuple_delimiter}"100m Sprint Record"{tuple_delimiter}"The World Athletics Federation is responsible for validating and recognizing new sprint records."{tuple_delimiter}"sports regulation, record certification"{tuple_delimiter}9){record_delimiter}
-("content_keywords"{tuple_delimiter}"athletics, sprinting, record-breaking, sports technology, competition"){completion_delimiter}
-#############################""",
+("entity"{tuple_delimiter}dwd_overspeed_info{tuple_delimiter}table{tuple_delimiter}创建的表)
+{record_delimiter}
+("entity"{tuple_delimiter}city_code{tuple_delimiter}field{tuple_delimiter}表字段)
+{record_delimiter}
+("entity"{tuple_delimiter}target_date{tuple_delimiter}field{tuple_delimiter}表字段)
+{record_delimiter}
+("entity"{tuple_delimiter}department_name{tuple_delimiter}field{tuple_delimiter}表字段)
+{record_delimiter}
+("relationship"{tuple_delimiter}dwd_overspeed_info{tuple_delimiter}city_code{tuple_delimiter}表和字段的关系{tuple_delimiter}10)
+{record_delimiter}
+("relationship"{tuple_delimiter}dwd_overspeed_info{tuple_delimiter}city_code{tuple_delimiter}表和字段的关系{tuple_delimiter}10)
+{record_delimiter}
+("relationship"{tuple_delimiter}dwd_overspeed_info{tuple_delimiter}city_code{tuple_delimiter}表和字段的关系{tuple_delimiter}10)
+{record_delimiter}
+{completion_delimiter}
+""",
 ]
+
 
 PROMPTS[
     "summarize_entity_descriptions"
@@ -150,14 +178,15 @@ Description List: {description_list}
 Output:
 """
 
+
 PROMPTS["entity_continue_extraction"] = """
 MANY entities and relationships were missed in the last extraction.
 
 ---Remember Steps---
 
 1. Identify all entities. For each identified entity, extract the following information:
-- entity_name: Name of the entity, use same language as input text. If English, capitalized the name.
-- entity_type: One of the following types: [{entity_types}]
+- entity_name: Name of the entity, use same language as input text.
+- entity_type: One of the following types: [{entity_type}]
 - entity_description: Comprehensive description of the entity's attributes and activities
 Format each entity as ("entity"{tuple_delimiter}<entity_name>{tuple_delimiter}<entity_type>{tuple_delimiter}<entity_description>)
 
@@ -182,6 +211,7 @@ Format the content-level key words as ("content_keywords"{tuple_delimiter}<high_
 Add them below using the same format:\n
 """.strip()
 
+
 PROMPTS["entity_if_loop_extraction"] = """
 ---Goal---'
 
@@ -192,9 +222,11 @@ It appears some entities may have still been missed.
 Answer ONLY by `YES` OR `NO` if there are still entities that need to be added.
 """.strip()
 
+
 PROMPTS["fail_response"] = (
     "Sorry, I'm not able to provide an answer to that question.[no-context]"
 )
+
 
 PROMPTS["rag_response"] = """---Role---
 
@@ -229,6 +261,7 @@ When handling relationships with timestamps:
 - Addtional user prompt: {user_prompt}
 
 Response:"""
+
 
 PROMPTS["keywords_extraction"] = """---Role---
 
@@ -329,6 +362,7 @@ When handling content with timestamps:
 - Addtional user prompt: {user_prompt}
 
 Response:"""
+
 
 # TODO: deprecated
 PROMPTS[
